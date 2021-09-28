472f499ecf12ac579a3c298d0531019b10f35f7c
pingles-ascend <kyhkkoko@gmail.com>
2021-09-28 17:20:43 +0900

    third mod

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,9 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/publish/<int:pk>', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('drafts/<int:pk>', views.post_draft_detail, name='post_draft_detail'),
+    path('drafts/<int:pk>/remove/', views.post_draft_remove, name='post_draft_remove'),
 ]
